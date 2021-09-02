99fc742e2b6cfebbb0bcd1864d78008534a958a6
Mental2devil <72179080+Mental2devil@users.noreply.github.com>
2021-09-02 18:03:14 +0530

    Update proposal.js

--- proposal.js
+++ proposal.js
@@ -1,1 +1,15 @@
+//propse a girl using js
 
+let myLove = prompt("Do You Love Me Say Yes OR say No");
+if (myLove=="yes"){
+  console.log("I Love You Too");
+}
+else if (myLove=="Yes"){
+  console.log("I Love You Too");
+}
+else if (myLove=="YES"){
+    console.log("I Love You Too");
+}
+else {
+  console.log("Get lost");
+}
